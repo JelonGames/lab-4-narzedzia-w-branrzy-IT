6f62c95cca5cb0a28dfa360b83a0776d30d2ffc1
JelonGames <jelonyt@gmail.com>
2022-07-04 20:41:40 +0200

    init

--- git_kontrol.sh
+++ git_kontrol.sh
@@ -16,10 +16,13 @@ Logs () {
 	fi
 
 	for ((i = 1; i <= ${temp}; i++)); do
+		mkdir log$i
+		cd log$i
 		touch log$i.txt
 		echo log$i.txt > log$i.txt
 		echo $0 >> log$i.txt
 		echo $(Date --date now) >> log$i.txt
+		cd ..
 	done
 }
 
@@ -29,9 +32,17 @@ Help (){
 	echo --date wyświetla date
 }
 
+Init () {
+	git clone https://github.com/JelonGames/lab-4-narzedzia-w-branrzy-IT.git
+	rm -d -r -f lab-4-narzedzia-w-branrzy-IT/ >> PATH
+	export PATH
+	echo $PATH
+}
+
 case "$opt" in
 	"--date" | "-d") Date_view;;
 	"--logs" | "-l") Logs;;
 	"--help" | "-h") Help;;
+	"--init" | "-i") Init;;
 	*) echo  nie znaleziono polecenia $opt
 esac
